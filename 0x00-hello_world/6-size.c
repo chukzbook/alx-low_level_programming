c86dbd778c15d9b7270679bdf7a6fc4cef96ace9
Chukwuma okeke <chukzbooks@gmail.com>
2023-02-17 17:11:30 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %lu byte(s)\n', (unsigned long)sizeof(a));
-printf("Size of a char: %lu byte(s)\n', (unsigned long)sizeof(b));
-printf("Size of a char: %lu byte(s)\n', (unsigned long)sizeof(c));
-printf("Size of a char: %lu byte(s)\n', (unsigned long)sizeof(d));
-printf("Size of a char: %lu byte(s)\n', (unsigned long)sizeof(e));
-return (0)
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+return (0);
 }
